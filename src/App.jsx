e51f840e46556a36b70a30f027ced519f1f4ac81
bill kiplimo <billkiplimo1@gmail.com>
2025-02-24 12:02:59 +0300

    added some features

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,38 @@
 // import './App.css';
-import Fist from './Fist';
- 
+
  function App(){
 
-    // const name = "maxie";
-    // let age = 20;
-    // //window.alert(`hello ${name} welcome again ${age}`);
-    // return(
-    //     <div>
-
-
-    //         <nav >
-
-    //             <ul className='nav'>
-    //                 <li><a href="#">services</a></li>
-    //                 <li><a href="#">services</a></li>
-    //                 <li><a href="#">services</a></li>
-    //                 <li><a href="#">services</a></li>
-    //                 <li><a href="#">services</a></li>
-                    
-    //             </ul>
-    //         </nav>
-    //         <div className="maxi">
-    //             <h1>hello {name} you are {age}</h1>
-    //             <label htmlFor="name">fName</label>
-    //             <input type="text" className='fName' />
-    //         <li>services</li>
-    //             <li>services</li>
-    //             <li>services</li>
-    //             <li>services</li>
-    //             <li>services</li>
-    //         </div>
-
-    //         </div>
+    const name = "maxie";
+    let age = 20;
+    //window.alert(`hello ${name} welcome again ${age}`);
     return(
-        <Fist/>
-    );
-        
+        <div>
+
+
+            <nav >
+
+                <ul className='nav'>
+                    <li><a href="#">services</a></li>
+                    <li><a href="#">services</a></li>
+                    <li><a href="#">services</a></li>
+                    <li><a href="#">services</a></li>
+                    <li><a href="#">services</a></li>
+
+                </ul>
+            </nav>
+            <div className="maxi">
+                <h1>hello {name} you are {age}</h1>
+                <label htmlFor="name">fName</label>
+                <input type="text" className='fName' />
+                <li>services</li>
+                <li>services</li>
+                <li>services</li>
+                <li>services</li>
+                <li>services</li>
+            </div>
+</div >
+    )
+
     
  }
- export default App
+ export default App;
